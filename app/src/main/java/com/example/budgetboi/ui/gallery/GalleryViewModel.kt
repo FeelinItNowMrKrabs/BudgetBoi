fb2728b23dac42899ee9b341ab79a54dd0ce38fd
Peter Yakovlev <yakovlev123peter@gmail.com>
2021-03-11 21:22:31 +0500

    Added funny meme haha ayy lmao

--- app/src/main/java/com/example/budgetboi/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/budgetboi/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is gallery poop fart ayy lmao"
     }
     val text: LiveData<String> = _text
 }
